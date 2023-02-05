7ed5ec708e39f9f9691021eb3b1bc7e94015cf04
Olivia Jain <olivia.jain@gmail.com>
2023-02-04 23:36:48 -0800

    Second commit to add templates and recipes

--- flask_app.py
+++ flask_app.py
@@ -5,12 +5,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def index(): 
-    print(recipes)
     return render_template('index.html', recipes=list(recipes.keys()))
 
-@app.route('/chili_tofu')
-def chili_tofu(): 
-    return render_template("recipe.html", recipe=recipes.get('Chili Tofu'))
+@app.route('/recipes/<recipe_name>')
+def recipe(recipe_name):
+    return render_template("recipe.html", recipe=recipes.get(recipe_name))
 
 if __name__ == '__main__':
     app.run(debug=True)
--- recipes.py
+++ recipes.py
@@ -1,5 +1,6 @@
 recipes = { "Chili Tofu": {
                 'name': 'Chili Tofu',
+                'image': 'chili_tofu.jpg',
                 'ingredients': [
                     'soy sauce', 
                     'sriracha',
@@ -10,7 +11,7 @@ recipes = { "Chili Tofu": {
                     'onion', 
                     'green bell pepper', 
                     'spinach',
-                    'chili powder or butter chicken spice mixture',
+                    'chili powder',
                     'cornstarch'
                 ],
                 'instructions': [
@@ -24,6 +25,79 @@ recipes = { "Chili Tofu": {
                     'Deglaze the pan with the sauce mixture and add a handful of spinach.',
                     'Reduce to medium heat and cook until sauce thickens.',
                     'Serve once thickened, about 7 minutes.'
+                ]
+            },
+            "Chickpea Curry": {
+                'name': 'Chickpea Curry',
+                'image': 'chickpea_curry.jpg',
+                'ingredients': [
+                    'garbanzo beans', 
+                    'frozen naan',
+                    'onion', 
+                    'coconut milk',
+                    'tomato sauce',
+                    'yellow curry powder'
+                ],
+                'instructions': [
+                    'Dice 1/2 onion and saute in oil. This can be done in a large pot or instant pot.',
+                    'After 5 minutes or until translucent, power in a heaping amount of yellow curry powder (should cover most of the bottom).'
+                    'Open your garbanzo bean can and strain. Pour in, and toast lightly for a few minutes.', 
+                    'Add 2 big spoons of tomato sauce and pour coconut milk until the color turns a light rich yellow.',
+                    'Increase the heat to bring the liquid to a boil, and close the lid. For the instant pot pressure cook on high.'
+                    'Let sit for 7 minutes. Meanwhile, broil your frozen naan.',
+                    'When chickpeas are slightly softened, the curry is ready to eat. If you need the liquid to thicken, add cornstarch and stir.', 
+                    'Garnish with cilantro or enjoy as is.'
+                ]
+            },
+            "Creamy Ramen": {
+                'name': 'Creamy Ramen',
+                'image': 'creamy_ramen.jpg',
+                'ingredients': [
+                    'mushrooms', 
+                    'tofu',
+                    'cherry tomatoes', 
+                    'corn',
+                    'carrots', 
+                    'spinach', 
+                    'noodles',
+                    'veggie broth',
+                    'coconut milk',
+                    'soy sauce',
+                    'miso paste'
+                ],
+                'instructions': [
+                    'Prep: Slice your mushrooms. Slice a few slabs of tofu. Press and pat dry. Chop carrots and halve cherry tomatoes.',
+                    'Soak tofu in soy sauce and airfry or bake at 400 for 10 minutes.',
+                    'Saute mushrooms in oil for 5 minutes then add soy sauce. Heat for 5 minutes or until mushrooms are dark brown.',
+                    'Prepare the broth: Pour 1 part veggie broth, 1 huge spoon of miso paste, and roughly a 1/2 cup of soy sauce into a big pot. Bring to a boil.',
+                    'Add in your carrots to soften. Add the noodles in the broth and follow instructions for preparation.',
+                    'Two minutes before the noodles are done, add a handful of spinach and your corn. Pour in coconut milk until the broth looks opaque.',
+                    'Serve in a big bowl, and garnish with cherry tomatoes, tofu, mushrooms, and cilantro.'
+                ]
+            },
+             "Hearty Orzo": {
+                'name': 'Hearty Orzo',
+                'image': 'hearty_orzo.jpg',
+                'ingredients': [
+                    'mushrooms', 
+                    'onion',
+                    'bell pepper', 
+                    'cherry tomatoes', 
+                    'corn',
+                    'spinach', 
+                    'orzo',
+                    'veggie broth',
+                    'coconut milk',
+                    'soy sauce'                
+                ],
+                'instructions': [
+                    'Dice 1/2 onion, slice a handful of mushrooms, and dice 1 bell pepper. Saute in oil in a big pan.',
+                    'Add a splash of soy sauce and heat 5 minutes or until translucent.',
+                    'Pour in 1 cup of veggie broth, another spoon of soy sauce, and just enough water for the orzo and bring to a boil.',
+                    'Add in your orzo and follow instructions. You may keep adding veggie broth or water as the orzo absorbs. Stir constantly.',
+                    'Halve your cherry tomatoes.',
+                    'About 2 minutes before the orzo is done, add in corn and spinach. Add coconut milk until opaque. Mix in cherry tomatoes.',
+                    'Taste the broth for richness and adjust to your liking. Enjoy.'
                 ]
             }
         }
