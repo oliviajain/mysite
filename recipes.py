6e43e8560b0b0a0711ae10eb7d88b603c3daf8c6
Olivia Jain <olivia.jain@gmail.com>
2023-02-04 16:35:38 -0800

    update with recipe

--- recipes.py
+++ recipes.py
@@ -1,27 +1,29 @@
-recipes = { "Chocolate Cake": {
-                'name': 'Chocolate Cake',
+recipes = { "Chili Tofu": {
+                'name': 'Chili Tofu',
                 'ingredients': [
-                    '2 cups all-purpose flour',
-                    '1 1/2 cups sugar',
-                    '3/4 cup unsweetened cocoa powder',
-                    '2 teaspoons baking powder',
-                    '1 1/2 teaspoons baking soda',
-                    '1 teaspoon salt',
-                    '2 eggs',
-                    '1 cup milk',
-                    '1/2 cup vegetable oil',
-                    '2 teaspoons vanilla extract',
-                    '1 1/2 cups boiling water'
+                    'soy sauce', 
+                    'sriracha',
+                    'ketchup',
+                    'maple syrup', 
+                    'lime juice', 
+                    'tofu', 
+                    'onion', 
+                    'green bell pepper', 
+                    'spinach',
+                    'chili powder or butter chicken spice mixture',
+                    'cornstarch'
                 ],
                 'instructions': [
-                    'Preheat oven to 350 degrees F (175 degrees C).',
-                    'Grease and flour two 9-inch round cake pans.'
-                    'In a large bowl, whisk together flour, sugar, cocoa powder, baking powder, baking soda, and salt.',
-                    'Add eggs, milk, oil, and vanilla extract; beat on medium speed of mixer for 2 minutes.',
-                    'Stir in boiling water (batter will be thin).',
-                    'Pour batter evenly into prepared pans.',
-                    'Bake 30 to 35 minutes, or until a toothpick inserted into the center of the cake comes out clean.',
-                    'Cool completely.'
+                    'Preheat the oven or airfryer to 400 degrees. Press your tofu and dice. Dice 1/2 an onion and one bell pepper.', 
+                    'Mix the 1 spoon of cornstarch with 1 spoon of the spices mixture.', 
+                    'Coat your tofu with the dry spices mixture and bake or fry for 15 minutes.',
+                    'Prepare the sauce mixture. Mix 3 spoons of soy sauce with 1 spoon of sriracha, 1 big spoon of ketchup, and pour lime juice in until the consistency is runny. Add in a splash of maple syrup to thicken.',
+                    'Saute the diced onion and bell pepper for about 5 minutes.',
+                    'Add in the spice mixture to toast the spices.', 
+                    'Add tofu to the pan and mix for 2 minutes.', 
+                    'Deglaze the pan with the sauce mixture and add a handful of spinach.',
+                    'Reduce to medium heat and cook until sauce thickens.',
+                    'Serve once thickened, about 7 minutes.'
                 ]
             }
         }
