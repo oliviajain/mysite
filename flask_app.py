54dcad8c52b1340834aef676bd8c392e054e5ec5
Olivia Jain <olivia.jain@gmail.com>
2023-02-08 09:38:43 -0800

    Add checkbox form

--- flask_app.py
+++ flask_app.py
@@ -1,16 +1,42 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request, redirect
 from recipes import recipes 
 
 app = Flask(__name__)
 
 def get_all_ingredients(): 
     all_ingredients = set()
-    for recipe_name, recipe in recipes.items(): 
+    for _, recipe in recipes.items(): 
         all_ingredients.update(recipe['ingredients'])
     return all_ingredients
 
-@app.route('/')
+def get_top_recipes(selected_ingredients): 
+    ingredients = set(selected_ingredients)
+    info = {}
+    counts = []
+    for recipe_name, recipe in recipes.items():
+        recipe_ingredients = set(recipe['ingredients'])
+        overlap = recipe_ingredients.intersection(ingredients)
+        missing = recipe_ingredients.difference(ingredients)
+        info[recipe_name] = {'missing': missing}
+        counts.append((len(overlap), recipe_name))
+    counts.sort(key=lambda x: x[0], reverse=True)
+    tops = counts[:3]
+    return {top_recipe[1]: list(info.get(top_recipe[1]).get('missing')) for top_recipe in tops}
+       
+@app.route("/handle_form_submit", methods=['POST'])
+def handle_form_submit():
+    selected_ingredients = request.form.getlist("selected_options")
+    tops = get_top_recipes(selected_ingredients)
+    return tops
+
+@app.route('/', methods=['GET', 'POST'])
 def index(): 
+    if request.method == 'POST':
+        selected_choices = []
+        for choice in ['choice1', 'choice2', 'choice3']:
+            if choice in request.form:
+                selected_choices.append(request.form[choice])
+        return str(selected_choices)
     ingredients = get_all_ingredients()
     return render_template('index.html', recipes=list(recipes.keys()), ingredients=list(ingredients))
 
